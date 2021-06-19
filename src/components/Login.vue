9ba777d92d20adeed60b7cea108cd9320ef0b352
KYOSG <kaiyuanosg@outlook.com>
2021-06-19 19:58:14 +0800

    2021.6.19 19:58

---
+++ src/components/Login.vue
@@ -0,0 +1,90 @@
+<template>
+<div class="login_background">
+  <div class="login_box">
+    <div class="logo_box">
+      <img src="../assets/logo.png" alt="">
+    </div>
+    <!--登录表单-->
+    <el-form :model="loginform" class="form">
+
+      <el-form-item >
+        <el-input v-model="loginform.username" placeholder="请输入用户名"
+                  prefix-icon="el-icon-user-solid"></el-input>
+      </el-form-item>
+      <el-form-item >
+        <el-input v-model="loginform.password" placeholder="请输入密码" type="password"
+                  prefix-icon="el-icon-key"></el-input>
+      </el-form-item>
+
+      <el-form-item class="button">
+        <el-button type="primary">登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      loginform:{
+        username:'',
+        password:''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.login_background {
+  height: 100%;
+  background-color: rgb(0,78,100);
+}
+
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #E8E8E8;
+  border-radius: 15px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,  -50%);
+
+}
+
+.logo_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid white;
+  border-radius: 50%;
+  padding: 10px;
+  box-shadow: 0 0 10px rgba(0,0,0,0.4);
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #E8E8E8;
+}
+
+img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: white;
+}
+
+.form{
+  padding-left: 22%;
+  padding-right: 20%;
+  padding-top: 23%;
+
+}
+.button{
+  padding-left: 1%;
+}
+</style>
