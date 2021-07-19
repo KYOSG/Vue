ef84aad951818ad7b74095b1d997370d0e4a3739
KYOSG <kaiyuanosg@outlook.com>
2021-07-19 20:46:52 +0800

    2021.7.19 20:46 注册页面基本完成

--- src/assets/Js/signUp.js
+++ src/assets/Js/signUp.js
@@ -1,20 +1,22 @@
 import '../css/signup.css'
-const subjectOptions = ['物理', '化学', '生物', '政治','历史','地理'];
+import { defineComponent, ref } from 'vue'
+//const subjectOptions = ['物理', '化学', '生物', '政治','历史','地理'];
 export default {
     name: "Signup",
     data() {
         return {
             labelPosition: 'right',
             studentData: {
-                name: '',
-                password:'',
-                score: '',
-                type: '',
-                year: '',
+                name: ref(''),
+                number:ref(''),
+                password:ref(''),
+                score: ref(''),
+                type: ref(''),
+                year: ref(''),
                 subject: [''],
             },
 
-            subjects: subjectOptions
+           // subjects: subjectOptions
         };
     }
 }
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,29 +6,32 @@
   <div class="From-Background">
 <div class="From">
   <el-form :label-position="labelPosition" label-width="80px" >
-    <!--用户名-->
-   <el-form-item label="姓名" class="fromItem">
-      <el-input v-model="studentData.name"></el-input>
+    <!--姓名-->
+   <el-form-item label="姓名" class="fromItem" >
+      <el-input v-model="studentData.name" clearable></el-input>
     </el-form-item>
+
+    <!--考号-->
+    <el-form-item label="考生号" class="fromItem">
+      <el-input v-model="studentData.number" clearable></el-input>
+    </el-form-item>
+
     <!--密码-->
     <el-form-item label="密码" class="fromItem">
     <el-input v-model="studentData.password"
               type="password"
               prefix-icon="el-icon-key"
-              show-password></el-input>
+              show-password clearable></el-input>
     </el-form-item>
-    <!--成绩-->
-    <el-form-item label="高考成绩" class="fromItem">
-      <el-input v-model="studentData.score"></el-input>
-    </el-form-item>
+
     <!--毕业时间-->
-    <el-form-item label="毕业时间">
+    <el-form-item label="毕业时间" class="formItem">
         <el-date-picker
             v-model="studentData.year"
             type="year">
         </el-date-picker>
     </el-form-item>
-    <!--高考选课-->
+    <!--高考选课
     <el-form-item label="所选科目">
       <el-checkbox-group
           v-model="studentData.subject"
@@ -38,10 +41,15 @@
         <el-checkbox-button v-for="subject in subjects" :label="subject" :key="subject">{{subject}}</el-checkbox-button>
       </el-checkbox-group>
     </el-form-item>
-
+-->
   </el-form>
 
 </div>
+
+    <el-row>
+       <el-button type="primary" class="submit_button">提交</el-button>
+      <el-button type="primary" class="clear_button">清空</el-button>
+    </el-row>
 
   </div>
 
