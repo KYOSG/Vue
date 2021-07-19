fedd2865e4be4142eddbadfa22315c76d9583792
KYOSG <kaiyuanosg@outlook.com>
2021-07-19 22:08:57 +0800

    2021.7.19 22:08 院校库选择CheckBox选择功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,18 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from "@/views/Login";
 import Home from "@/components/Home";
 import Signup from "@/views/Signup";
+import searchSchool from "@/components/searchSchool";
 
 
 
 
 
 const routes = [
-  {path:'/',redirect:'/Home'},
+  {path:'/',redirect:'/searchSchool'},
   {path: '/Login',name:'Login',component:Login},
   {path: '/Home', name:'Home',component:Home},
-  {path: '/Signup', name:'Signup',component:Signup}
-
+  {path: '/Signup', name:'Signup',component:Signup},
+  {path: '/searchSchool', name:'searchSchool',component:searchSchool}
 
 ]
 
