30b0a6a3fc85091a7cc74237155ee7253b5999e0
UDK_KL <kaiyuanosg@outlook.com>
2021-08-24 22:23:10 +0800

    2021.8.24 22:22 1.更新了部分插件 2.更新了部分页面

--- src/main.js
+++ src/main.js
@@ -2,5 +2,38 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+import './assets/css/global.css'
+import 'lib-flexible'
+import axios from 'axios';
+import echarts from 'echarts'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+installElementPlus(app)
+
+app.use(store).use(router).mount('#app')
+
+app.config.globalProperties.$http = axios;
+//接口请求的基准路径
+axios.defaults.baseURL = 'http://192.168.1.111:8083/';
+// 添加请求拦截器
+
+axios.interceptors.request.use(config => {
+    // 在发送请求之前做些什么
+
+    // 判断是否存在token,如果存在将每个页面header添加token
+    if (sessionStorage.getItem("token")) {
+        config.headers.username = sessionStorage.getItem("username");
+        config.headers.identity = sessionStorage.getItem("identity");
+        config.headers.token = sessionStorage.getItem("token");
+    }
+
+    return config
+})
+
+
+//Echarts
+app.config.globalProperties.$echarts = window.echarts;
+
+app.config.globalProperties.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,116 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
-const routes = [
+import { createRouter, createWebHashHistory} from 'vue-router'
+import Login from "@/components/Login";
+import Home from "@/components/Home";
+import Signup from "@/components/Signup";
+import SchoolSel from "@/views/StudentHome/SchoolSel";
+import ManagerHome from "@/components/ManageHome";
+import ManagerWelcome from "@/views/ManagerHome/ManagerWelcome";
+import StudentHome from"@/components/StudentHome"
+import StudentInfManager from "@/views/ManagerHome/StudentInfManager";
+import TeacherInfManager from "@/views/ManagerHome/TeacherInfManager";
+import StudentWelcome from "@/views/StudentHome/StudentWelcome"
+import SchoolInfManager from "@/views/ManagerHome/SchoolInfManager";
+import MajorSel from "@/views/StudentHome/MajorSel";
+import MajorInf from "@/views/StudentHome/MajorInf";
+import SchoolInf from "@/views/StudentHome/SchoolInf";
+import Applications from "@/views/StudentHome/Applications";
+import SchoolPublic from "@/views/Home/SchoolPublic";
+import MajorPublic from "@/views/Home/MajorPublic";
+const routes = [{
+  path: '/',
+  redirect: '/Home'
+},
   {
-    path: '/',
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/Home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/Signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/SchoolPublic',
+    name: 'SchoolPublic',
+    component: SchoolPublic
+  },
+  {
+    path: '/MajorPublic',
+    name: 'MajorPublic',
+    component: MajorPublic
+  },
+  //管理员界面
+  {
+    path: '/ManagerHome',
+    name: 'ManagerHome',
+    component: ManagerHome,
+    redirect: '/ManagerWelcome',
+    children: [{
+      path: '/ManagerWelcome',
+      component: ManagerWelcome
+    },{
+      path: '/StudentInfManager',
+      component: StudentInfManager,
+    },{
+      path: '/TeacherInfManager',
+      component: TeacherInfManager,
+    },{
+      path: '/SchoolInfManager',
+      component: SchoolInfManager,
+    },]
+  },
+    //学生界面
+  {
+    path: '/StudentHome',
+    name: 'StudentHome',
+    component: StudentHome,
+    redirect: '/StudentWelcome',
+    children: [{
+      path: '/StudentWelcome',
+      component: StudentWelcome,
+    },{
+      path: '/SchoolSel',
+      component: SchoolSel,
+    },{
+      path: '/MajorSel',
+      component: MajorSel,
+    },{
+      path: '/SchoolInf',
+      component: SchoolInf
+    },{
+      path: '/MajorInf',
+      component: MajorInf
+    },{
+      path: '/Applications',
+      component: Applications
+    }]
+  },
+
+  //老师界面
+  ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
 
+//导航守卫
+//开启前先做白名单
+/*
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login')
+    return next();
+
+  const tokenStr = window.sessionStorage.getItem('token');
+
+  if (!tokenStr)
+    return next('/login');
+})
+*/
 export default router
